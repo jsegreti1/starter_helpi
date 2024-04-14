49aa4500550db1226d5edbeeb275ede60e7c8fd9
Aaron Xue <axue@udel.edu>
2024-04-14 16:18:33 -0400

    Added homepage description, Basic and Detailed questions, and related state behavior

---
+++ src/BasicQuestions.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BasicQuestions: React.FC = () => {
+  return (
+    <div>
+      <h2>Basic Questions</h2>
+      <p>This is where you'll find some basic questions to get you started.</p>
+      {/* You can add more content here as needed */}
+    </div>
+  );
+};
+
+export default BasicQuestions;
