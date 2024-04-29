ddc4aababc9b3bf11cbc3dfc7ff1caa566a40bb1
Aaron Xue <axue@udel.edu>
2024-04-28 20:55:58 -0400

    Testing to try to get around secret block

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import { BasicQuestions } from './BasicQuestions';
 import { DetailedQuestions } from './DetailedQuestions';
 
 
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
+// Key stored locally
 let keyData = "";
 const saveKeyData = "MYKEY";
-const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+const prevKey = localStorage.getItem(saveKeyData);
 if (prevKey !== null) {
   keyData = JSON.parse(prevKey);
 }
